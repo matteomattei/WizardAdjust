866a22726214a12cee67bbecc29317c10d20a751
Matteo Mattei <matteo.mattei@gmail.com>
2013-01-14 21:08:50 +0100

    Add header and support for linux

--- wizardadjust.py
+++ wizardadjust.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python2
 
 import sys, os, datetime, re, csv
 import Tkinter, tkMessageBox
@@ -18,8 +18,14 @@ def copyfile(file1,file2):
 	f.write(buf)
 	f.close()
 
-DESTINATION_DIR = os.path.join(os.environ['USERPROFILE'], 'WizardAdjust','results')
-STANDARD_FILE = os.path.join(os.environ['USERPROFILE'], 'WizardAdjust','defstd.csv')
+DESTINATION_DIR = ''
+STANDARD_FILE = ''
+if sys.platform=='win32':
+    DESTINATION_DIR = os.path.join(os.environ['USERPROFILE'], 'WizardAdjust','results')
+    STANDARD_FILE = os.path.join(os.environ['USERPROFILE'], 'WizardAdjust','defstd.csv')
+elif sys.platform.startswith('linux'):
+    DESTINATION_DIR = '/tmp/results'
+    STANDARD_FILE = os.path.join(os.getcwd(),'defstd.csv')
 
 ### This is necessary to hide background TK box ###
 window = Tkinter.Tk()
@@ -82,22 +88,27 @@ if sys.version_info >= (3,0,0):
 else:
     f = open(dest_file, 'wb')
 writer = csv.writer(f, delimiter=';', quotechar='"', quoting=csv.QUOTE_ALL)
+header = ['Action','Sample ID','True value(mg/l)','Conc (mg/lt)','Abs.','DF','SG#','Date','Time','Actual conc(mg/l)']
+writer.writerow(header)
 conc_std = 0
 ass_std = 0
 dil_std = 0
 for line in lines:
+    if DEBUG: print(line)
     std = isStandard(element, line[1], standards)
     if std[0]==True:
         conc_std = float(std[1])
         ass_std = float(line[4])
         dil_std = float(line[5])
+        line.append(conc_std)
         writer.writerow(line)
         continue
     if ass_std == 0 or dil_std == 0:
+        line.append(conc_std)
         writer.writerow(line)
         continue
     if DEBUG: print('((conc_std=%d * ass=%d) / ass_std=%d) * (dil=%d / dil_std=%d)' % (conc_std, float(line[4]), ass_std, float(line[5]), dil_std))
     conc = (conc_std * float(line[4]) / ass_std) * (float(line[5]) / dil_std)
-    line[4] = conc
+    line.append(conc)
     writer.writerow(line)
 f.close()
